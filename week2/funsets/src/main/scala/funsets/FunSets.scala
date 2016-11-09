fd76ffef1e448efa58a9dd5f16d09a43e45f7a65
Daniel Brice <dbrice@cj.com>
2016-11-08 16:02:57 -0800

    Done with progfun1-funsets
    done with week 2

--- week2/funsets/src/main/scala/funsets/FunSets.scala
+++ week2/funsets/src/main/scala/funsets/FunSets.scala
@@ -19,32 +19,35 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
-  
+  def singletonSet(elem: Int): Set =
+    (i: Int) => i == elem
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
-  
+  def union(s: Set, t: Set): Set =
+    (i: Int) => s(i) || t(i)
+
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
-  
+  def intersect(s: Set, t: Set): Set =
+    (i: Int) => s(i) && t(i)
+
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
-  
+  def diff(s: Set, t: Set): Set =
+    (i: Int) => s(i) && !t(i)
+
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
-  
+  def filter(s: Set, p: Int => Boolean): Set =
+    (i: Int) => s(i) && p(i)
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -54,26 +57,22 @@ object FunSets {
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-    def forall(s: Set, p: Int => Boolean): Boolean = {
-    def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
-    }
-    iter(???)
-  }
-  
+  def forall(s: Set, p: Int => Boolean): Boolean =
+    1.to(bound).forall(i => !s(i) || p(i))
+
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
-  
+  def exists(s: Set, p: Int => Boolean): Boolean =
+    1.to(bound).exists(i => s(i) && p(i))
+
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
-  
+  def map(s: Set, f: Int => Int): Set =
+    (i: Int) => s(f(i))
+
   /**
    * Displays the contents of a set
    */
